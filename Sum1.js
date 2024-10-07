d53fe96f28822764f9fba5342eb693ae8a8d481f
SAYITHA-M <sayithamicheal@gmail.com>
2024-10-07 23:36:40 +0530

    Add files via upload

---
+++ Sum1.js
@@ -0,0 +1,12 @@
+let totalAmount =100;
+ let egg12 =50;
+ let tomatto1 =15;
+ let potatto2 =20;
+ let shopOwnertips = 5;
+ let childTips =10;
+let  remaining= 0;
+// console.log(egg12
+let grocery=(egg12 +tomatto1 +  potatto2 );
+console.log(grocery);
+console.log(childTips);
+console.log(remaining);
