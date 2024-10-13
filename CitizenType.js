9ea8325169d95b5feb6fa1501a4a12f8ee7ce734
SAYITHA-M <sayithamicheal@gmail.com>
2024-10-13 22:57:12 +0530

    Add files via upload

---
+++ CitizenType.js
@@ -0,0 +1,13 @@
+const age = 98;
+
+if (typeof (age) == 'number') {
+    if (age >= 60) {
+        console.log('senior');
+    }
+    else {
+        console.log('junior');
+    }
+}
+else {
+    console.log('only enter the numbers');
+}
